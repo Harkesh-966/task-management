7fdb92e1b2de043a83d919c62f3c60375a1853e5
Harkesh-966 <harkeshsaini108@gmail.com>
2025-08-28 18:17:11 +0530

    add delete icon

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'tasks' },
-  { path: 'auth', loadChildren: () => import('./features/auth/routes').then(m => m.AUTH_ROUTES) },
-  { path: 'tasks', canActivate: [authGuard], loadChildren: () => import('./features/tasks/routes').then(m => m.TASK_ROUTES) },
-  { path: '**', loadComponent: () => import('./shared/not-found/not-found.component').then(c => c.NotFoundComponent) }
+    { path: '', pathMatch: 'full', redirectTo: 'tasks' },
+    { path: 'auth', loadChildren: () => import('./features/auth/routes').then(m => m.AUTH_ROUTES) },
+    { path: 'tasks', canActivate: [authGuard], loadChildren: () => import('./features/tasks/routes').then(m => m.TASK_ROUTES) },
+    { path: '**', loadComponent: () => import('./shared/not-found/not-found.component').then(c => c.NotFoundComponent) }
 ];
