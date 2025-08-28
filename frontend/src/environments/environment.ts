e4d567f61a6c13578fd2b9fd389f61fd12d4899b
Harkesh-966 <harkeshsaini108@gmail.com>
2025-08-29 00:05:37 +0530

    finish assesment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://w3idki55pi.execute-api.ap-south-1.amazonaws.com/api',
+    apiBaseUrl: 'http://localhost:4000/api',
+    // apiBaseUrl: 'https://w3idki55pi.execute-api.ap-south-1.amazonaws.com/api',
 };
